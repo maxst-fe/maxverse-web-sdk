5c7717577a398109fbf239e553c4fd0ac1f87697
ken <bhchoi@maxst.com>
2023-06-29 15:45:18 +0900

    feat(participant/targetParticipant): wrap participant instance to targetParticipant class #2

--- packages/media/src/participant/targetParticipant.ts
+++ packages/media/src/participant/targetParticipant.ts
@@ -1,4 +1,4 @@
-import { LocalParticipant, Participant, RemoteParticipant } from 'livekit-client';
+import { LocalParticipant, Participant, RemoteParticipant, Track } from 'livekit-client';
 
 export class TargetParticipantFactory {
   static createTargetParticipant = (participant: Participant) => {
@@ -16,4 +16,51 @@ export class TargetParticipant {
   constructor(participant: Participant) {
     this.#participant = participant;
   }
+
+  get sid() {
+    return this.#participant.sid;
+  }
+
+  get identity() {
+    return this.#participant.identity;
+  }
+
+  get name() {
+    return this.#participant.name;
+  }
+
+  get isLocal() {
+    return this.#participant instanceof LocalParticipant;
+  }
+
+  get #videoTrackPublication() {
+    return this.#participant.getTrack(Track.Source.Camera);
+  }
+
+  get #audioTrackPublication() {
+    return this.#participant.getTrack(Track.Source.Microphone);
+  }
+
+  get isVideoEnabled() {
+    const isSubscribed = this.#videoTrackPublication?.isSubscribed ?? false;
+    const isValidTrack = this.#videoTrackPublication?.videoTrack ?? false;
+
+    return isSubscribed && isValidTrack && this.#participant.isCameraEnabled;
+  }
+
+  get isAudioEnabled() {
+    const isSubscribed = this.#audioTrackPublication?.isSubscribed ?? false;
+    const isValidTrack = this.#audioTrackPublication?.audioTrack ?? false;
+
+    return isSubscribed && isValidTrack && this.#participant.isMicrophoneEnabled;
+  }
+
+  attachElement = (element: HTMLMediaElement) => {
+    if (element instanceof HTMLVideoElement && this.isVideoEnabled) {
+      this.#videoTrackPublication?.videoTrack?.attach(element);
+    }
+    if (element instanceof HTMLAudioElement && this.isAudioEnabled) {
+      this.#videoTrackPublication?.audioTrack?.attach(element);
+    }
+  };
 }
