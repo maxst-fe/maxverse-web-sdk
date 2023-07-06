c4b0c674c6be67408471291de96f6797aa7584be
ken <bhchoi@maxst.com>
2023-07-06 18:24:46 +0900

    feat(helper/dataChannelContainer): add dataChannelContainer for handles data messages

---
+++ packages/media/src/helper/dataChannelContainer.ts
@@ -0,0 +1,53 @@
+import { Participant, Room } from 'livekit-client';
+import Presenter from './presenter';
+
+export enum MetadataSort {
+  PRESENTER = 'presenter',
+  UNKNOWN = 'unknown',
+}
+export type MetadataValue = {
+  [MetadataSort.PRESENTER]: { userId: string };
+};
+export type Metadata = Record<MetadataSort, unknown>;
+
+class DataChannelContainer {
+  #presenter: Presenter;
+
+  constructor(room: Room, presenter: Presenter) {
+    this.#presenter = presenter;
+  }
+
+  #parse = <T extends Metadata | undefined>(data: string) => {
+    return JSON.parse(data) as T;
+  };
+
+  #checkSort = (data: Metadata, sort: MetadataSort) => {
+    return Object.hasOwn(data, sort);
+  };
+
+  onFetchMetaDataHandler = (metadata: string): { type: MetadataSort; payload?: Participant } => {
+    const parsedMetadata = this.#parse(metadata);
+
+    if (!parsedMetadata) {
+      console.error('not found valid metadata');
+      return { type: MetadataSort.UNKNOWN };
+    }
+
+    if (this.#checkSort(parsedMetadata, MetadataSort.PRESENTER)) {
+      const { userId } = parsedMetadata.presenter as MetadataValue[MetadataSort.PRESENTER];
+
+      const presenter = this.#presenter.find(userId);
+
+      if (!presenter) {
+        console.error('do not found presenter');
+        return { type: MetadataSort.UNKNOWN };
+      }
+
+      return { type: MetadataSort.PRESENTER, payload: presenter };
+    }
+
+    return { type: MetadataSort.UNKNOWN };
+  };
+}
+
+export default DataChannelContainer;
