aa4c19ee3108ecffa7769f796d6676dd90ec49e7
ken <bhchoi@maxst.com>
2023-11-07 11:17:57 +0900

    feat(editor-match-react/hooks): add required custom hook to entry file

--- packages/editor-match-react/src/hooks/index.ts
+++ packages/editor-match-react/src/hooks/index.ts
@@ -2,3 +2,5 @@ export * from './usePointMaterial';
 export * from './useSyncInfo';
 export * from './useEditor';
 export * from './useEditorEvents';
+export * from './useMarkerEvents';
+export * from './useMatchService';
