ef9a941990da8ea897771ec779278f053fc9e8c4
ken <bhchoi@maxst.com>
2023-11-23 14:08:24 +0900

    feat(resize/ResizeService): sovled the problem of ply data being distorted in the scene

--- packages/editor/src/editor/service/resize/ResizeService.ts
+++ packages/editor/src/editor/service/resize/ResizeService.ts
@@ -1,23 +1,15 @@
-import * as THREE from "three";
-import { WebGLRenderer } from "three";
+import * as THREE from 'three';
+import { WebGLRenderer } from 'three';
 
 export interface IResizeService {
-  setResize(
-    renderer: THREE.WebGLRenderer,
-    canvas: HTMLCanvasElement,
-    camera: THREE.PerspectiveCamera
-  ): void;
+  setResize(renderer: THREE.WebGLRenderer, canvas: HTMLCanvasElement, camera: THREE.PerspectiveCamera): void;
 }
 class ResizeService implements IResizeService {
-  public setResize(
-    renderer: WebGLRenderer,
-    canvas: HTMLCanvasElement,
-    camera: THREE.PerspectiveCamera
-  ): void {
+  public setResize(renderer: WebGLRenderer, canvas: HTMLCanvasElement, camera: THREE.PerspectiveCamera): void {
     const width = canvas.clientWidth || 1;
     const height = canvas.clientHeight || 1;
-    console.log(canvas.clientWidth);
-    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.aspect = width / height;
     camera.updateProjectionMatrix();
 
     renderer.setPixelRatio(window.devicePixelRatio);
